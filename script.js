a5e37f2cbd34a20ed3ac4f703be654167ae3f2dd
Luciano Pires <lucianopires98@outlook.com>
2022-06-30 19:47:09 -0300

    1º TRABALHO DE INGLÊS!

---
+++ script.js
@@ -0,0 +1,32 @@
+var movieComedy = [{
+    title: 'Law Angels',
+    director: 'Phil Lord and Chris Miller',
+    year: 2012
+}
+];
+
+
+var movieAction = [{
+    title: 'The Purge',
+    director: 'James Demonaco',
+    year: 2013
+}
+];
+
+
+var movieFantasy = [{
+    title: 'Warcraft',
+    director: 'Duncan Jones',
+    year: 2016
+}
+];
+
+
+let movie = 'Warcraft'
+
+
+if (movie == 'The Purge') {
+   console.log('My Favorit Movie Is: The Purge');
+} else {
+    console.log('Not Even 5 Stars'); 
+}
